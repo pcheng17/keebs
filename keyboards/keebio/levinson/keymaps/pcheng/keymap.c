28adb11d632161516102604f7f75f4ddbd8e13fc
Peter Cheng <pcheng17@gmail.com>
2021-12-27 23:39:30 -0800

    Replace enter with space, add more keys to navigation layer, lower layer is now empty

--- keyboards/keebio/levinson/keymaps/pcheng/keymap.c
+++ keyboards/keebio/levinson/keymaps/pcheng/keymap.c
@@ -60,28 +60,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     * - Left moves to function layer when held
     */
     [_QWERTY] = LAYOUT_ortho_4x12(
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,               KC_T,    KC_Y,    KC_U,               KC_I,              KC_O,    KC_P,    KC_BSPC, \
-        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,               KC_G,    KC_H,    KC_J,               KC_K,              KC_L,    KC_SCLN, KC_QUOT, \
-        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,               KC_B,    KC_N,    KC_M,               KC_COMM,           KC_DOT,  KC_SLSH, KC_ENT , \
-        XXXXXXX,        KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_DEL),  LT(_NAVI, KC_SPC),  KC_ENT,  LT(_RAISE, KC_BSPC), LT(_FNC, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT  \
+        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,                KC_T,               KC_Y,    KC_U,                KC_I,              KC_O,    KC_P,    KC_BSPC, \
+        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,                KC_G,               KC_H,    KC_J,                KC_K,              KC_L,    KC_SCLN, KC_QUOT, \
+        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,                KC_B,               KC_N,    KC_M,                KC_COMM,           KC_DOT,  KC_SLSH, KC_ENT , \
+        XXXXXXX,        KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_DEL),  LT(_NAVI, KC_SPC),  KC_SPC,  LT(_RAISE, KC_BSPC), LT(_FNC, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT  \
     ),
 
     /* Lower
     * .-----------------------------------------.      .-----------------------------------------.
     * |      |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  |      |
     * |------+------+------+------+------+------|      |------+------+------+------+------+------|
-    * |      |Hangul| Snip | WS<- | WS-> |      |      | Left | Down |  Up  |Right |      |      |
+    * |      |      |      |      |      |      |      |      |      |      |      |      |      |
     * |------+------+------+------+------+------|      |------+------+------+------+------+------|
     * | Caps |      |      |      |      |      |      |      |      |      |      |      |      |
     * |------+------+------+------+------+------|      |------+------+------+------+------+------|
-    * |      |      |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |
+    * |      |      |      |      |      |      |      |      |      |      |      |      |      |
     * '-----------------------------------------'      '-----------------------------------------'
     */
     [_LOWER] = LAYOUT_ortho_4x12( \
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-        _______, KC_HAEN, WINSNIP, WSLEFT,  WSRIGHT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
     ),
 
     /* Raise
@@ -98,15 +98,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     [_RAISE] = LAYOUT_ortho_4x12( \
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_PIPE, KC_BSLASH, _______, \
         _______, KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  LEQ,     _______, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,   KC_GRV,  \
-        _______, DUBCOL,  ARROW,   KC_LT,   KC_GT,   GEQ,     _______, KC_LCBR, KC_RCBR, KC_SLSH, KC_QUES, _______, \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+        _______, DUBCOL,  ARROW,   KC_LT,   KC_GT,   GEQ,     _______, KC_LCBR, KC_RCBR, KC_SLSH, KC_QUES,   _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______  \
     ),
 
     /* Navigation
     * .-----------------------------------------.      .-----------------------------------------.
-    * |      |      |      |      |      |      |      |      |      |  Up  |      |      |      |
+    * |      |      |      |      |      |      |      | PgUp | Home |  Up  | End  |      |      |
     * |------+------+------+------+------+------|      |------+------+------+------+------+------|
-    * |      |      | Snip | WS<- | WS-> |      |      |      | Left | Down |Right |      |      |
+    * |      |Hangul| Snip | WS<- | WS-> |      |      | PgDn | Left | Down |Right |      |      |
     * |------+------+------+------+------+------|      |------+------+------+------+------+------|
     * |      |      |      |      |      |      |      |      |      |      |      |      |      |
     * |------+------+------+------+------+------|      |------+------+------+------+------+------|
@@ -114,8 +114,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     * '-----------------------------------------'      '-----------------------------------------'
     */
     [_NAVI] = LAYOUT_ortho_4x12( \
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______, \
-        _______, _______, WINSNIP, WSLEFT,  WSRIGHT, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+        _______, _______, _______, _______, _______, _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______, \
+        _______, KC_HAEN, WINSNIP, WSLEFT,  WSRIGHT, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
     ),
