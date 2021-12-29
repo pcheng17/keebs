f51351f5b744ddcef040482e69049a6a74c5266a
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2021-12-29 01:28:37 -0800

    Replace Raise tap's backspace with enter

--- keyboards/keebio/levinson/keymaps/pcheng/keymap.c
+++ keyboards/keebio/levinson/keymaps/pcheng/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     * |------+------+------+------+------+------|      |------+------+------+------+------+------|
     * | Shift|   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /  |Enter |
     * |------+------+------+------+------+------|      |------+------+------+------+------+------|
-    * |      | Ctrl | GUI  | Alt  |Lower*|Space*|      |Enter |Raise*|Left* | Down |  Up  |Right |
+    * |      | Ctrl | GUI  | Alt  |Lower*|Space*|      |Space |Raise*|Left* | Down |  Up  |Right |
     * '-----------------------------------------'      '-----------------------------------------'
     *
     * - Ctrl acts as Esc when tapped
@@ -60,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     * - Left moves to function layer when held
     */
     [_QWERTY] = LAYOUT_ortho_4x12(
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,                KC_T,               KC_Y,    KC_U,                KC_I,              KC_O,    KC_P,    KC_BSPC, \
-        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,                KC_G,               KC_H,    KC_J,                KC_K,              KC_L,    KC_SCLN, KC_QUOT, \
-        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,                KC_B,               KC_N,    KC_M,                KC_COMM,           KC_DOT,  KC_SLSH, KC_ENT , \
-        XXXXXXX,        KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_DEL),  LT(_NAVI, KC_SPC),  KC_SPC,  LT(_RAISE, KC_BSPC), LT(_FNC, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT  \
+        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,                KC_T,               KC_Y,    KC_U,               KC_I,              KC_O,    KC_P,    KC_BSPC, \
+        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,                KC_G,               KC_H,    KC_J,               KC_K,              KC_L,    KC_SCLN, KC_QUOT, \
+        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,                KC_B,               KC_N,    KC_M,               KC_COMM,           KC_DOT,  KC_SLSH, KC_ENT , \
+        XXXXXXX,        KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_DEL),  LT(_NAVI, KC_SPC),  KC_SPC,  LT(_RAISE, KC_ENT), LT(_FNC, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT  \
     ),
 
     /* Lower
