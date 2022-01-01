ab665a6c732ab1ecde52fd9261fe6edc142e48e4
Peter Cheng <pcheng17@gmail.com>
2022-01-01 12:48:21 -0800

    Change how layers are enumerated

--- keyboards/keebio/levinson/keymaps/pcheng/keymap.c
+++ keyboards/keebio/levinson/keymaps/pcheng/keymap.c
@@ -1,20 +1,17 @@
 #include QMK_KEYBOARD_H
 
+enum layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _NAVI,
+    _NUMPAD,
+    _FNC,
+    _COLEMAK,
+    _ADJUST
+};
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY     0
-#define _LOWER      1
-#define _RAISE      2
-#define _NAVI       3
-#define _NUMPAD     4
-#define _FNC        5
-#define _COLEMAK_DH 6
-#define _ADJUST     16
-
-enum custom_keycodes
+enum keycodes
 {
     QWERTY = SAFE_RANGE,
     COLEMAK,
