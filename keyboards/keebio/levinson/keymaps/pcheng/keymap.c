0923dae20fc29a39d9097babe737805a4845c6f1
Peter Cheng <pcheng17@gmail.com>
2022-01-05 12:07:27 -0800

    Make lower key prefer hold action

--- keyboards/keebio/levinson/keymaps/pcheng/keymap.c
+++ keyboards/keebio/levinson/keymaps/pcheng/keymap.c
@@ -246,9 +246,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record)
 {
     switch (keycode) {
+        case LT(_LOWER, KC_DEL):
+            // Since my lower key is used to access characters, and it also acts as
+            // "Delete" when tapped, I need it to prefer the hold action if another
+            // key is pressed because chances are, I'm not trying to press "Delete"
+            // in the middle of a bunch of keystrokes.
         case LT(_RAISE, KC_ENT):
-            // Since my raise key is currently used to access symbols and it also acts
-            // as "Enter" when tapped, I need it prefer the hold action if another
+            // Since my raise key is used to access characters, and it also acts as
+            // "Enter" when tapped, I need it to prefer the hold action if another
             // key is pressed because chances are, I'm not trying to press "Enter"
             // in the middle of a bunch of keystrokes.
             return true;
