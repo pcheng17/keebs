ed01b02db2c6dd7ff03241a214eb1c16df453326
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2022-01-08 00:06:52 -0800

    Add home-row modifiers (#1)

--- keyboards/keebio/levinson/keymaps/pcheng/keymap.c
+++ keyboards/keebio/levinson/keymaps/pcheng/keymap.c
@@ -57,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     * - Left arrow enables function layer when held
     */
     [_QWERTY] = LAYOUT_ortho_4x12(
-        KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,                KC_T,               KC_Y,    KC_U,               KC_I,              KC_O,    KC_P,    KC_BSPC, \
-        LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,                KC_G,               KC_H,    KC_J,               KC_K,              KC_L,    KC_SCLN, KC_QUOT, \
-        KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,                KC_B,               KC_N,    KC_M,               KC_COMM,           KC_DOT,  KC_SLSH, KC_ENT , \
-        XXXXXXX,        KC_LCTL, KC_LGUI, KC_LALT, LT(_LOWER, KC_DEL),  LT(_NAVI, KC_SPC),  KC_SPC,  LT(_RAISE, KC_ENT), LT(_FNC, KC_LEFT), KC_DOWN, KC_UP,   KC_RGHT  \
+        KC_TAB,         KC_Q,    KC_W,         KC_E,         KC_R,                KC_T,               KC_Y,    KC_U,         KC_I,              KC_O,            KC_P,            KC_BSPC, \
+        LCTL_T(KC_ESC), KC_A,    LALT_T(KC_S), LSFT_T(KC_D), LCTL_T(KC_F),        KC_G,               KC_H,    LCTL_T(KC_J), RSFT_T(KC_K),      LALT_T(KC_L),    RGUI_T(KC_SCLN), KC_QUOT, \
+        KC_LSFT,        KC_Z,    KC_X,         KC_C,         KC_V,                KC_B,               KC_N,    KC_M,         KC_COMM,           KC_DOT,          KC_SLSH,         KC_ENT , \
+        XXXXXXX,        KC_LCTL, KC_LGUI,      KC_LALT,      LT(_LOWER, KC_DEL),  LT(_NAVI, KC_SPC),  KC_SPC,  RAISE,        LT(_FNC, KC_LEFT), KC_DOWN,         KC_UP,           KC_RGHT  \
     ),
 
     /* Lower
@@ -269,6 +269,14 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record)
 bool get_permissive_hold(uint16_t keycode, keyrecord_t *record)
 {
     switch (keycode) {
+        case LGUI_T(KC_A):
+        case LALT_T(KC_S):
+        case LSFT_T(KC_D):
+        case LCTL_T(KC_F):
+        case LCTL_T(KC_J):
+        case RSFT_T(KC_K):
+        case LALT_T(KC_L):
+        case RGUI_T(KC_SCLN):
         case LT(_NAVI, KC_SPC):
             // Since my left space is used to access my navigation keys when held,
             // I tend to accidentally trigger some of the navigation keys when I'm
