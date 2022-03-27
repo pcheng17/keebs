e96e3739a82a384ba40cca16497ff101eec70ebe
Peter Cheng <pcheng17@gmail.com>
2022-03-27 11:13:36 -0700

    Add new crkbd keymap for mac

--- keyboards/crkbd/keymaps/pcheng_mac/config.h
+++ keyboards/crkbd/keymaps/pcheng_mac/config.h
@@ -22,13 +22,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define USE_MATRIX_I2C
 
+/* Select hand configuration */
 
-// Select hand configuration
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
 #define USE_SERIAL_PD2
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
